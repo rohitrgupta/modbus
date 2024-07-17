aec60fcb4328e360863670ca6e54bd3b3941cea5
Rohit Gupta <rohitrgupta@gmail.com>
2024-07-17 16:57:57 +0000

    more public variables

--- payload.go
+++ payload.go
@@ -6,50 +6,50 @@ type Value struct {
 }
 
 type MbPayload struct {
-	vars  map[int]MbVar
-	start int
-	size  int
+	Vars  map[int]MbVar
+	Start int
+	Size  int
 }
 
 func NewMbPayload() MbPayload {
 	r := MbPayload{}
-	r.vars = make(map[int]MbVar)
-	r.start = 0
-	r.size = 0
+	r.Vars = make(map[int]MbVar)
+	r.Start = 0
+	r.Size = 0
 	return r
 }
 
 func (r *MbPayload) AddVariable(address int, v MbVar) {
-	if len(r.vars) == 0 {
-		r.start = address
+	if len(r.Vars) == 0 {
+		r.Start = address
 	}
-	if address < r.start {
-		r.size += 2 * (r.start - address)
-		r.start = address
+	if address < r.Start {
+		r.Size += 2 * (r.Start - address)
+		r.Start = address
 	}
-	r.vars[address] = v
+	r.Vars[address] = v
 	var size int
 	switch v.Fmt {
 	case "uint16", "int16":
-		size = 2 * (address - r.start + 1)
+		size = 2 * (address - r.Start + 1)
 	case "uint32", "int32", "float32":
-		size = 2 * (address - r.start + 2)
+		size = 2 * (address - r.Start + 2)
 	}
-	if r.size < size {
-		r.size = size
+	if r.Size < size {
+		r.Size = size
 	}
 }
 
-func (r *MbPayload) regToVar(start int, reg []byte) []Value {
-	if len(reg) < r.size {
+func (r *MbPayload) RegToVar(start int, reg []byte) []Value {
+	if len(reg) < r.Size {
 		return nil
 	}
-	val := make([]Value, len(r.vars))
+	val := make([]Value, len(r.Vars))
 	ctr := 0
 	for i := 0; i < len(reg); i += 2 {
 		address := start + i/2
-		if _, ok := r.vars[address]; ok {
-			v := r.vars[address]
+		if _, ok := r.Vars[address]; ok {
+			v := r.Vars[address]
 			switch v.Fmt {
 			case "uint16", "int16":
 				v.SetReg(reg[i : i+2])
@@ -68,7 +68,7 @@ func (r *MbPayload) regToVar(start int, reg []byte) []Value {
 				i += 2
 			}
 			ctr += 1
-			r.vars[address] = v
+			r.Vars[address] = v
 		}
 	}
 	return val
